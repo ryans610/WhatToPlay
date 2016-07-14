72a728dddaa53635895a2cfdaad6f34b73216b02
ryans610 <ryans610@gmail.com>
2016-07-14 17:15:26 +0800

    Add LocalStorage Save and Load

--- script.js
+++ script.js
@@ -7,9 +7,25 @@ function addGameToList(game){
     list.appendChild(g);
 }
 
+function loadData(){
+    var data=localStorage.gameList;
+    if(data!=undefined){
+        data.split(",").map(addGameToList);
+    }
+}
+
+function saveData(){
+    var data=Array.prototype.slice.call(list.getElementsByTagName("li")).map(function(game){
+        return game.innerText;
+    }).join(",");
+    console.log(data);
+    localStorage.gameList=data;
+}
+
 document.getElementById("add-game").addEventListener("click",function(){
     addGameToList(newGame.value);
     newGame.value="";
+    saveData();
 });
 
 document.getElementById("go").addEventListener("click",function(){
@@ -19,3 +35,5 @@ document.getElementById("go").addEventListener("click",function(){
     r=Math.floor(r);
     alert(l[r].innerHTML);
 });
+
+loadData();
